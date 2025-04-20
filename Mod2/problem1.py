26afa7305071b5c97ff56b08df6aa2619eaabfa1
Andres Reyes Rosa <j.reyesrosa3@gmail.com>
2025-04-19 23:38:41 -0700

    Update problem1.py

--- Mod2/problem1.py
+++ Mod2/problem1.py
@@ -1,1 +1,10 @@
-
+'''Create a program that prints the following output to the screen:
+"Water. Earth. Fire. Air. Long ago, the four nations lived together 
+in harmony. Then, everything changed when the Fire Nation attacked."
+'''
+avatar = (
+        "Water. Earth. Fire. Air.\n"
+        "Long ago, the four nations lived together in harmony.\n"
+        "Then, everything changed when the Fire Nation attacked."
+        )
+print(avatar)
