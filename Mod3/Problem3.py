30e41724026bd855ded382948dc6375b3986620f
Andres Reyes Rosa <j.reyesrosa3@gmail.com>
2025-04-27 19:42:05 -0700

    Update Problem3.py

--- Mod3/Problem3.py
+++ Mod3/Problem3.py
@@ -1,1 +1,6 @@
+''' 3. Prompt the user for a sentence and a word to try to find in that sentence. 
+Have the program print out whether the word was found in the sentence. (i.e. True or False)'''
 
+str1 = input("Write me a sentence - ")
+str2 = input("Give me a word to find in the sentence - ")
+print( str2 in str1)
