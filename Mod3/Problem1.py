2e3381fce0a8d9e7a03f3f9a25102a0ffdaf9b3d
Andres Reyes Rosa <j.reyesrosa3@gmail.com>
2025-04-27 19:41:20 -0700

    Update Problem1.py

--- Mod3/Problem1.py
+++ Mod3/Problem1.py
@@ -1,1 +1,6 @@
-
+''' Prompt the user for a word. Then, prompt the user for how many times they'd like that word repeated.
+Use the skills you learned in this module to print the word the correct number of times.'''
+str1 = input("What is a word that you would like to get repeated? ")
+str2 = int(input(" How many times you would like the word to be repeated? "))
+word = (str1 + ' ') * str2
+print( word)
